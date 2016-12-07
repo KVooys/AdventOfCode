742e97dde4f5898af3b8c5443c50733b2600762e
Kurt <Kvooys@hotmail.com>
2016-12-07 17:21:01 +0100

    Part 1 done! no clue what to do for part 2, though.

---
+++ AdventOfCode/Day7.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace AdventOfCode
+{
+    class Day7
+    {
+        static string[] lines = File.ReadAllLines(@"C: \Users\Kurt\Documents\Visual Studio 2015\Projects\AdventOfCode\input7.txt");
+        static string test = "uxpvoytxfazjjhi[qogwhtzmwxvjwxreuz]zduoybbzxigwggwu[lamifchqqwbphhsqnf]qrjdjwtnhsjqftnqsk[bsqinwypsnnvougrs]wfmhtjkysqffllakru";
+        static string betweenbracks = "";
+        static string nobracks = "";
+        static string substr1 = "";
+        static string substr2 = "";
+        static int count = 0;
+
+        public static void Main()
+        {
+            foreach(string s in lines)
+            {
+                RemoveBrackets(s);
+                //only count occurrences where the betweenbracks do not produce abba, but the nobracks does
+                if (CheckIfAbba(betweenbracks) == false)
+                {
+                    if(CheckIfAbba(s))
+                    {
+                        count++;
+                    }
+                }
+                betweenbracks = "";
+            }
+            Console.WriteLine(count);
+            Console.ReadLine();
+        }
+
+        public static string RemoveBrackets(string s)
+        {
+            //find the part(s) between bracks and remove them
+            MatchCollection bracks = Regex.Matches(s, @"\[(\w+)\]");
+            foreach (Match m in bracks)
+            {
+                betweenbracks += m.ToString();
+                nobracks = s.Replace(m.ToString(), "");
+            }    
+            return nobracks;
+        }
+
+        public static bool CheckIfAbba(string s)
+        {
+            for(int i = 0; i < s.Length-3; i++)
+            {
+                //make 2 substrings out of the first 4 characters to compare
+                substr1 = s.Substring(i, 2);
+                substr2 = s.Substring(i + 2, 2);
+
+                //check if tge substrings are inverse of one another and are not all the same letter
+                if (substr1[0].Equals(substr2[1]) && (substr2[0].Equals(substr1[1])) && !(substr1[0].Equals(substr1[1])))
+                {
+                    Console.WriteLine(substr1 + substr2);                  
+                    return true;
+                }
+                
+            }
+            return false;
+        }
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day6.Main();
+            Day7.Main();
         }       
     }
 }
