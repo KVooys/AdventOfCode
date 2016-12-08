9dc3aa7a05ad9df42712f2eb857eb61d9b6213cf
Kurt <Kvooys@hotmail.com>
2016-12-08 18:24:21 +0100

    Day 7 updated to make more sense, working on 7 part 2

--- AdventOfCode/Day7.cs
+++ AdventOfCode/Day7.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     class Day7
     {
         static string[] lines = File.ReadAllLines(@"C: \Users\Kurt\Documents\Visual Studio 2015\Projects\AdventOfCode\input7.txt");
-        static string test = "uxpvoytxfazjjhi[qogwhtzmwxvjwxreuz]zduoybbzxigwggwu[lamifchqqwbphhsqnf]qrjdjwtnhsjqftnqsk[bsqinwypsnnvougrs]wfmhtjkysqffllakru";
+        //static string test = "uxpvoytxfazjjhi[qogwhtzmwxvjwxreuz]zduoybbzxigwggwu[lamifchqqwbphhsqnf]qrjdjwtnhsjqftnqsk[bsqinwypsnnvougrs]wfmhtjkysqffllakru";
         static string betweenbracks = "";
         static string nobracks = "";
         static string substr1 = "";
@@ -20,13 +20,18 @@ namespace AdventOfCode
 
         public static void Main()
         {
-            foreach(string s in lines)
+            foreach (string s in lines)
             {
+                //put the part between brackets in betweenbracks
                 RemoveBrackets(s);
-                //only count occurrences where the betweenbracks do not produce abba, but the nobracks does
+                //put the rest in nobracks
+                nobracks = Regex.Replace(s, @"\[(\w+)\]", "");
+                Console.WriteLine(nobracks);
+                
+                //only count occurrences where the betweenbracks do not produce abba, but the entire string does
                 if (CheckIfAbba(betweenbracks) == false)
                 {
-                    if(CheckIfAbba(s))
+                    if (CheckIfAbba(s))
                     {
                         count++;
                     }
@@ -39,32 +44,50 @@ namespace AdventOfCode
 
         public static string RemoveBrackets(string s)
         {
-            //find the part(s) between bracks and remove them
+            //find the part(s) between bracks and put them in betweenbracks
             MatchCollection bracks = Regex.Matches(s, @"\[(\w+)\]");
             foreach (Match m in bracks)
             {
-                betweenbracks += m.ToString();
-                nobracks = s.Replace(m.ToString(), "");
-            }    
-            return nobracks;
+                betweenbracks += m.ToString();                               
+            }
+            return s;           
         }
 
         public static bool CheckIfAbba(string s)
         {
-            for(int i = 0; i < s.Length-3; i++)
+            for (int i = 0; i < s.Length - 3; i++)
             {
                 //make 2 substrings out of the first 4 characters to compare
                 substr1 = s.Substring(i, 2);
                 substr2 = s.Substring(i + 2, 2);
 
-                //check if tge substrings are inverse of one another and are not all the same letter
+                //check if the substrings are inverse of one another and are not all the same letter
                 if (substr1[0].Equals(substr2[1]) && (substr2[0].Equals(substr1[1])) && !(substr1[0].Equals(substr1[1])))
                 {
-                    Console.WriteLine(substr1 + substr2);                  
+                    Console.WriteLine(substr1 + substr2);
                     return true;
                 }
-                
             }
+            return false;
+        }
+
+        public static bool CheckAba(string s)
+        {
+            for (int i = 0; i < s.Length - 3; i++)
+            {
+                //make 2 substrings out of the first 3 characters to compare
+                substr1 = s.Substring(i, 2);
+                substr2 = ""+ s[i + 2];
+
+                //check if the substrings are inverse of one another and are not all the same letter
+                if (substr1[0].Equals(substr2[1]) && (substr2[0].Equals(substr1[1])) && !(substr1[0].Equals(substr1[1])))
+                {
+                    Console.WriteLine(substr1 + substr2);
+                    return true;
+                }
+            }
+                
+
             return false;
         }
     }
---
+++ AdventOfCode/Day8.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode
+{
+    class Day8
+    {
+        static char[,] LCD = new char[3, 7]
+          {
+                {'.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.'},
+                {'.', '.', '.', '.', '.', '.', '.'}
+          };
+
+        public static void Main()
+        {
+            Rect(3, 2);
+            MoveCol(2, 1);
+            //display the grid as it currently is
+            for (int k = 0; k < LCD.GetLength(0); k++)
+            {
+                
+                for(int l = 0; l < LCD.GetLength(1); l++)
+                {
+                    Console.Write(LCD[k, l]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        //create an x,y rectangle of #s
+        public static void Rect(int a, int b)
+        {
+            for (int i = 0; i < a; i++)
+            {
+                for (int j = 0; j < b; j++)
+                {
+                    LCD[j, i] = '#';
+                }
+            }
+        }
+
+        //shift the array at column [a] down by b
+        public static void MoveCol(int a, int b)
+        {
+            
+        }
+
+    }
+}
