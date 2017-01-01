37a5118212e74a8724ebb0f5399d7d17b24254c7
Kurt <Kvooys@hotmail.com>
2017-01-01 18:03:05 +0100

    Fixed few issues on Day9, not done yet!

---
+++ AdventOfCode/Day9.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode
+{
+    class Day9
+    {
+        static string source = File.ReadAllText(@"C: \Users\Kurt\Documents\Visual Studio 2015\Projects\AdventOfCode\input9.txt");
+        static string test = "A(1x5)BC";
+        static string[] betweenbracks;
+        static string fix;
+        static int beforex;
+        static int afterx;
+
+        public static void Main()
+        {
+            Console.WriteLine(test);
+            Decipher();
+            Console.ReadLine();
+        }
+
+        public static void Decipher()
+        {
+            //get the part between brackets
+            MatchCollection bracks = Regex.Matches(test, @"\((.+)\)");
+            foreach (Match m in bracks)
+            {
+                fix = m.ToString();
+                //remove brackets and x to find relevant numbers
+                fix = fix.Replace("(", "");
+                fix = fix.Replace(")", "");
+                betweenbracks = fix.Split('x');
+                beforex = int.Parse(betweenbracks[0]);
+                afterx = int.Parse(betweenbracks[1]);
+                Console.WriteLine(beforex + " " + afterx);
+
+                //now we need to check the input's next [afterx] characters right after the 2nd bracket
+            }
+            
+        }
+
+    }
+}
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -11,7 +11,7 @@ namespace AdventOfCode
     {
         static void Main(string[] args)
         {
-            Day7.Main();
+            Day9.Main();
         }       
     }
 }
